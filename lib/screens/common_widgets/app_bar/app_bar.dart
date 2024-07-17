bd07e0d18902941b7547d17556311a5582757d75
gadel <Khisamutdinov.g@gmail.com>
2024-07-17 11:47:50 +0400

    Внесение небольших правок

--- lib/screens/common_widgets/app_bar/app_bar.dart
+++ lib/screens/common_widgets/app_bar/app_bar.dart
@@ -12,13 +12,27 @@ class ShopAppBar extends StatelessWidget implements PreferredSizeWidget {
       leading: const Icon(Icons.accessibility_new_outlined),
       title: const Row(
         children: [
-          Text('Подборки'),
+          Text(
+            'Подборки',
+            style: TextStyle(fontSize: 13),
+          ),
           SizedBox(width: 15),
-          Text('Каталог'),
+          Text(
+            'Каталог',
+            style: TextStyle(fontSize: 13),
+          ),
         ],
       ),
       actions: [
-        const Text('data'),
+        const SizedBox(
+          width: 420,
+          height: 42,
+          child: TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
         const SizedBox(width: 80),
         GestureDetector(child: const Text('RU')),
         const VerticalDivider(indent: 17, endIndent: 17, color: Colors.black),
@@ -33,39 +47,3 @@ class ShopAppBar extends StatelessWidget implements PreferredSizeWidget {
     );
   }
 }
-
-// class Header extends StatelessWidget {
-//   const Header({
-//     super.key,
-//   });
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       children: [
-//         const Icon(
-//           Icons.abc,
-//         ),
-//         const SizedBox(width: 20),
-//         const Text('Подборки'),
-//         const SizedBox(width: 15),
-//         const Text('Каталог'),
-//         const SizedBox(width: 40),
-//         // const TextField(),
-//         const SizedBox(width: 20),
-//         GestureDetector(child: const Text('RU')),
-//         const VerticalDivider(
-//           width: 3,
-//           color: Colors.black,
-//         ),
-//         GestureDetector(
-//           child: const Text('EN'),
-//         ),
-//         const SizedBox(width: 15),
-//         const Icon(Icons.abc), const Text('0'),
-//         const SizedBox(width: 15),
-//         const Text('Войти'),
-//       ],
-//     );
-//   }
-// }
---
+++ lib/screens/menu_screen.dart/menu_screen_widgets/main_menu.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class MainMenu extends StatelessWidget {
+  const MainMenu({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 40,
+            vertical: 25,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Хиты продаж',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w200),
+                  ),
+                  SizedBox(width: 20),
+                  Text(
+                    'Смотреть все',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 20),
+              StaggeredGrid.count(
+                crossAxisCount: 12,
+                mainAxisSpacing: 20,
+                crossAxisSpacing: 20,
+                children: const [
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 4,
+                    mainAxisCellCount: 6,
+                    child: ProductCardOutside(),
+                  ),
+                  StaggeredGridTile.count(
+                      crossAxisCellCount: 2,
+                      mainAxisCellCount: 3,
+                      child: ProductCardOutside()),
+                  StaggeredGridTile.count(
+                      crossAxisCellCount: 2,
+                      mainAxisCellCount: 3,
+                      child: ProductCardOutside()),
+                  StaggeredGridTile.count(
+                      crossAxisCellCount: 4,
+                      mainAxisCellCount: 3,
+                      child: ProductCardOutside()),
+                  StaggeredGridTile.count(
+                      crossAxisCellCount: 2,
+                      mainAxisCellCount: 3,
+                      child: ProductCardOutside()),
+                  StaggeredGridTile.count(
+                      crossAxisCellCount: 2,
+                      mainAxisCellCount: 3,
+                      child: ProductCardOutside()),
+                  StaggeredGridTile.count(
+                      crossAxisCellCount: 2,
+                      mainAxisCellCount: 3,
+                      child: ProductCardOutside()),
+                  StaggeredGridTile.count(
+                      crossAxisCellCount: 2,
+                      mainAxisCellCount: 3,
+                      child: ProductCardOutside()),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ProductCardOutside extends StatelessWidget {
+  const ProductCardOutside({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed('/product');
+      },
+      child: DecoratedBox(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(8)),
+          color: Color(0xffe1e1e1),
+          image: DecorationImage(
+            image: AssetImage('assets/images/pic3.jpg'),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(12),
+              child: SizedBox(
+                width: 55,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(8),
+                      ),
+                      border: Border.all(color: const Color(0xffa6a4a4))),
+                  child: const Padding(
+                    padding: EdgeInsets.all(6),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('4.5'),
+                        SizedBox(width: 5),
+                        Icon(
+                          Icons.star,
+                          size: 17,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const Spacer(),
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Название товара',
+                    style: TextStyle(fontSize: 13, fontWeight: FontWeight.w200),
+                  ),
+                  Text(
+                    'Категория',
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                  Text(
+                    '50 Р',
+                    style: TextStyle(
+                      fontSize: 13,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
